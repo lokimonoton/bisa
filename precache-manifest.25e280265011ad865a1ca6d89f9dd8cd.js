cf55ce9af928eafab9cc5e2195eae615f44af9de
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-17 01:02:34 +0000

    Deploy lokimonoton/bisa to github.com/lokimonoton/bisa.git:gh-pages

---
+++ precache-manifest.25e280265011ad865a1ca6d89f9dd8cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86ec830688f95aced35e",
+    "url": "/bisa/static/css/main.11d3a30a.chunk.css"
+  },
+  {
+    "revision": "86ec830688f95aced35e",
+    "url": "/bisa/static/js/main.86ec8306.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/bisa/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "e78b80e4edddf6279b6d",
+    "url": "/bisa/static/js/runtime~main.e78b80e4.js"
+  },
+  {
+    "revision": "bc198aa2f2e6f66e7cf271eb1c972dfd",
+    "url": "/bisa/index.html"
+  }
+];
