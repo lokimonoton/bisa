455e27671c0ce379bef710e843e881dd56c9f3cd
lokimonoton <slafe1@yandex.com>
2018-12-17 01:23:30 +0000

    bismillah

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import Head from './head/Head';
+import Card from './card/Card';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-sadasdasdasdsa
+      <Head/>
+       <Grid container spacing={24}>
+        <Grid item xs={6}>
+          <Paper ><Card/></Paper>
+        </Grid>
+
+      </Grid>
+
       </div>
     );
   }
